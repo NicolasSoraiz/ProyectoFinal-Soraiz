d37684ca5b8995bd096527ac479de2aff0c18fb2
Nicolas Soraiz <soraizesteban9@gmail.com>
2025-11-29 17:12:02 -0300

    Correcion de errores

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import Navbar from "./components/navbar"
-import ItemsListContainer from "./components/ItemsListContainer"
+import Navbar from "./components/Navbar";
+import ItemsListContainer from "./components/ItemsListContainer";
 
 function App() {
   return (
